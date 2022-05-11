ca174026e8890a592a225d100cb11efd4dd82e0f
Jordan Neal <jordaneal@gmail.com>
2022-05-11 11:48:35 -0700

    Less tests

--- RocketTests/Tests.cs
+++ RocketTests/Tests.cs
@@ -6,53 +6,75 @@ namespace RocketTests
     [TestClass]
     public class Tests
     {
-        // failing tests
         [TestMethod]
-        public void SpeedTestFail() // speed test
+        public void FlyToMoonFail()
         {
             Rocket r = new Rocket();
 
             Assert.AreNotEqual(29000, r.Speed);
-        }
-        [TestMethod]
-        public void EngineTestPassFail() // engine test
-        {
-            Rocket r = new Rocket();
-
             Assert.AreNotEqual(true, r.EnginesAreWorking);
-        }
-        [TestMethod]
-        public void DirectionTestFail() // direction test
-        {
-            Rocket r = new Rocket();
-
             Assert.AreNotEqual("To the Moon!", r.Direction);
         }
-
-        // passing tests
         [TestMethod]
-        public void SpeedTestPass() // speed test
+        public void FlyToMoonPass()
         {
             Rocket r = new Rocket();
             r.FlyToTheMoon();
 
             Assert.AreEqual(29000, r.Speed);
-        }
-        [TestMethod]
-        public void EngineTestPass() // engine test
-        {
-            Rocket r = new Rocket();
-            r.FlyToTheMoon();
-
             Assert.AreEqual(true, r.EnginesAreWorking);
-        }
-        [TestMethod]
-        public void DirectionTestPass() // direction test
-        {
-            Rocket r = new Rocket();
-            r.FlyToTheMoon();
-
             Assert.AreEqual("To the Moon!", r.Direction);
         }
+
+        // failing tests
+        
+        //[TestMethod]
+        //public void SpeedTestFail() // speed test
+        //{
+        //    Rocket r = new Rocket();
+
+        //    Assert.AreNotEqual(29000, r.Speed);
+        //}
+        //[TestMethod]
+        //public void EngineTestPassFail() // engine test
+        //{
+        //    Rocket r = new Rocket();
+
+        //    Assert.AreNotEqual(true, r.EnginesAreWorking);
+        //}
+        //[TestMethod]
+        //public void DirectionTestFail() // direction test
+        //{
+        //    Rocket r = new Rocket();
+
+        //    Assert.AreNotEqual("To the Moon!", r.Direction);
+        //}
+
+        // passing tests
+        
+        //[TestMethod]
+        //public void SpeedTestPass() // speed test
+        //{
+        //    Rocket r = new Rocket();
+        //    r.FlyToTheMoon();
+
+        //    Assert.AreEqual(29000, r.Speed);
+        //}
+        //[TestMethod]
+        //public void EngineTestPass() // engine test
+        //{
+        //    Rocket r = new Rocket();
+        //    r.FlyToTheMoon();
+
+        //    Assert.AreEqual(true, r.EnginesAreWorking);
+        //}
+        //[TestMethod]
+        //public void DirectionTestPass() // direction test
+        //{
+        //    Rocket r = new Rocket();
+        //    r.FlyToTheMoon();
+
+        //    Assert.AreEqual("To the Moon!", r.Direction);
+        //}
     }
 }
